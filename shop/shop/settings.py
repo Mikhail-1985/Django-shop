072f7a6259910974229db8e58f7ec3300fac84be
Mikhail Knyazev <knyazevmo@gmail.com>
2020-10-25 13:09:14 +0300

    settings.py

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+from config import SECRET_DJANGO_KEY
 from pathlib import Path
 import os
 
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tnl3_n*x1fk#(8q^dpd7b5y#ojfrzexbr7^ob!3jt!k(s*58c8'
+SECRET_KEY = SECRET_DJANGO_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
